474479523755d4ff6fee3b1853c19ea02c8936d8
Brian <bjjx1999@live.com>
2018-08-19 13:34:24 -0700

    Scene Manager Stuff

---
+++ Assets/Scripts/PlatformScripts/BackgroundMusicManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusicManager : MonoBehaviour {
+
+    public AudioClip _backgroundMusic;
+    private AudioSource _audioSource;
+
+    void Awake()
+    {
+        _audioSource = this.gameObject.AddComponent<AudioSource>();
+    }
+
+    // Use this for initialization
+    void Start () {
+        if (_backgroundMusic != null)
+        {
+            _audioSource.clip = _backgroundMusic;
+            _audioSource.loop = true;
+            _audioSource.Play();
+        }
+	}
+}
---
+++ Assets/Scripts/UIScript/GameOverManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverManager : MonoBehaviour {
+
+    public GameObject Player;
+    public GameObject DeathText;
+    public GameObject RestartButton;
+
+    // Update is called once per frame
+    void Update () {
+		if (Player == null)
+        {
+            Time.timeScale = 0;
+            DeathText.SetActive(true);
+            RestartButton.SetActive(true);
+        }
+	}
+}
