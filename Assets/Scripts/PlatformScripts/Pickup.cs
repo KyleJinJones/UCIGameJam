133cd21ece517c130ca145fdb0132c9f1c9fd07e
Brian <bjjx1999@live.com>
2018-08-19 13:35:19 -0700

    Maybe Prefabs

--- Assets/Scripts/PlatformScripts/Pickup.cs
+++ Assets/Scripts/PlatformScripts/Pickup.cs
@@ -3,33 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Pickup : MonoBehaviour {
-//    public int staminagain = 30;
-//	// Use this for initialization
-//	void Start () {
-//		
-//	}
-//	
-//	// Update is called once per frame
-//	void Update () {
-//        
-//
-//   
-//    }
-//
-//    private void OnTriggerEnter2D(Collider2D other)
-//    {
-//        if(other.tag == "Player")
-//        {
-//            float stamina = other.GetComponent<StaminaBar>().Stamina;
-//            if (stamina - staminagain <0)
-//            {
-//                other.GetComponent<StaminaBar>().Stamina = 0;
-//            }
-//            else
-//            {
-//                other.GetComponent<StaminaBar>().Stamina -= staminagain;
-//            }
-//            Destroy(this.gameObject);
-//        }
-//    }
+    public int staminagain = 30;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            float stamina = other.GetComponent<StaminaBar>().Stamina;
+            if (stamina - staminagain < 0)
+            {
+                other.GetComponent<StaminaBar>().Stamina = 0;
+            }
+            else
+            {
+                other.GetComponent<StaminaBar>().Stamina -= staminagain;
+            }
+            Destroy(this.gameObject);
+        }
+    }
 }
